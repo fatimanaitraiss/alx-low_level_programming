ba3ce8dc33b6d53d1fa7daa40353031244d474a6
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-04-29 08:58:30 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,17 +9,17 @@
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *current;
+	listint_t *tmp;
 
-    if (head == NULL)
-        return;
+	if (head == NULL)
+		return;
 
-    while (*head != NULL)
-    {
-        current = *head;
-        *head = (*head)->next;
-        free(current);
-    }
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
 
-    *head = NULL;
+	head = NULL;
 }
