ea799d7616e956edef467f927cf665b0b093b912
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-05-08 09:13:10 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,18 +14,23 @@
  * Return: 1 on success, -1 on failure
  */
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
+int create_file(const char *filename, char *text_content)
+{
+    if (filename == NULL)
+    {
         return -1;
     }
     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         return -1;
     }
-    if (text_content != NULL) {
+    if (text_content != NULL)
+    {
         size_t len = strlen(text_content);
         ssize_t num_written = write(fd, text_content, len);
-        if (num_written != (ssize_t)len) {
+        if (num_written != (ssize_t)len)
+       	{
             close(fd);
             return -1;
         }
