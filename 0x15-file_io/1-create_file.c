e834d2bbd55b482c83900663a289a7d991dcc7e8
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-05-08 09:15:16 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+
 /**
  * create_file - function that creates a file.
  * @filename: variable pointer
