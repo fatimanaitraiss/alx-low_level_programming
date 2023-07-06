f90f86a2e530562d8ebd1a53e31ba521d77d11ed
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-07-06 15:01:44 -0700

    second commit

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,10 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -16,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -31,8 +30,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -42,12 +41,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-
-void replace_value(hash_node_t **ht, const char *key, const char *value);
-int check_key(hash_node_t *ht, const char *key);
-hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
-void print_list(hash_node_t *h);
-void free_list(hash_node_t *head);
 
 /**
  * struct shash_node_s - Node of a sorted hash table
@@ -92,12 +85,5 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-
-int replace_value_s(shash_node_t **ht, const char *key, const char *value);
-int check_key_s(shash_node_t *ht, const char *key);
-shash_node_t *add_node_s(shash_node_t **head,
-	const char *key, const char *value);
-void insert_sort(shash_node_t *node, shash_table_t *ht);
-void free_list_s(shash_node_t *head);
 
 #endif
